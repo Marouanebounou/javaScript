4712bfac2127408577fa08f389cec09560941e83
Marouane Bounou <maroinbounou.123@gmail.com>
2025-09-23 10:23:52 +0100

    challenge 1

---
+++ server.js
@@ -0,0 +1,14 @@
+let nom , prenom ,age , sexe ,num;
+
+nom = prompt("Entre votre nom")
+prenom = prompt("Entre votre prenom")
+age = prompt("Entre votre age")
+sexe = prompt("Entre votre sexe")
+num = prompt("Entre votre Phone Number")
+
+console.log(nom)
+console.log(prenom)
+console.log(age)
+console.log(sexe)
+console.log(num)
+
